d813669017f6a631e21eb6a9eb09e4396961c7e1
joklek <31686813+joklek@users.noreply.github.com>
2018-04-08 13:03:26 +0300

    Expand MaildropInfo and MessageInfo
    These could be merged somehow as they have similar patterns

--- POP3r/Pop3/ServerResponses/MaildropInfo.cs
+++ POP3r/Pop3/ServerResponses/MaildropInfo.cs
@@ -1,8 +1,34 @@
-﻿namespace POP3r.Pop3.ServerResponses
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace POP3r.Pop3.ServerResponses
 {
     public struct MaildropInfo
     {
-        public int NumberOfMessages { get; set; }
-        public int SizeInOctets { get; set; }
+        public int NumberOfMessages { get; }
+        public int SizeInOctets { get; }
+
+        public MaildropInfo(string responseBody)
+        {
+            if (!Regex.IsMatch(responseBody, @"^\d* \d*\s"))
+                throw new Exception("Incorrect response string provided: " + responseBody);
+
+            var splitResponse = responseBody.Split(' ');
+            var numberOfMessages = int.Parse(splitResponse[0]);
+            var sizeInOctets = int.Parse(splitResponse[1]);
+
+            if (numberOfMessages < 0 || sizeInOctets < 0)
+                throw new Exception("Maildrop info must be greater than zero. Number of Messages: " + numberOfMessages + ", Size of octets: " + sizeInOctets);
+            NumberOfMessages = numberOfMessages;
+            SizeInOctets = sizeInOctets;
+        }
+
+        public MaildropInfo(int numberOfMessages, int sizeInOctets)
+        {
+            if (numberOfMessages < 0 || sizeInOctets < 0)
+                throw new Exception("Maildrop info must be greater than zero. Number of Messages: " + numberOfMessages + ", Size of octets: " + sizeInOctets);
+            NumberOfMessages = numberOfMessages;
+            SizeInOctets = sizeInOctets;
+        }
     }
 }
--- POP3r/Pop3/ServerResponses/MessageInfo.cs
+++ POP3r/Pop3/ServerResponses/MessageInfo.cs
@@ -1,14 +1,34 @@
-﻿namespace POP3r.Pop3.ServerResponses
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace POP3r.Pop3.ServerResponses
 {
     public class MessageInfo
     {
-        public MessageInfo(int messageId, int sizeInOctets)
+        public int MessageId { get; }
+        public int SizeInOctets { get; }
+
+        public MessageInfo(string responseBody)
         {
+            if (!Regex.IsMatch(responseBody, @"^\d* \d*\s"))
+                throw new Exception("Incorrect response string provided: " + responseBody);
+
+            var splitResponse = responseBody.Split(' ');
+            var messageId = int.Parse(splitResponse[0]);
+            var sizeInOctets = int.Parse(splitResponse[1]);
+
+            if (messageId < 0 || sizeInOctets < 0)
+                throw new Exception("Maildrop info must be greater than zero. MessageId: " + messageId + ", Size of octets: " + sizeInOctets);
             MessageId = messageId;
             SizeInOctets = sizeInOctets;
         }
 
-        public int MessageId { get; }
-        public int SizeInOctets { get; }
+        public MessageInfo(int messageId, int sizeInOctets)
+        {
+            if (messageId < 0 || sizeInOctets < 0)
+                throw new Exception("Maildrop info must be greater than zero. MessageId: " + messageId + ", Size of octets: " + sizeInOctets);
+            MessageId = messageId;
+            SizeInOctets = sizeInOctets;
+        }
     }
 }
