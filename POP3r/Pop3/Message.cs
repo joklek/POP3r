843907590b51b29d28e7738073501bb61c25004a
joklek <31686813+joklek@users.noreply.github.com>
2018-04-08 14:36:10 +0300

    Message parsing from response

--- POP3r/Pop3/Message.cs
+++ POP3r/Pop3/Message.cs
@@ -1,4 +1,5 @@
-﻿using POP3r.Pop3.Interfaces;
+﻿using System;
+using POP3r.Pop3.Interfaces;
 using POP3r.Pop3.ServerResponses;
 
 namespace POP3r.Pop3
@@ -9,6 +10,20 @@ namespace POP3r.Pop3
         public string Body { get; set; }
         public bool IsComplete { get; set; }
         public MessageInfo Info { get; }
+        public int SizeInOctets { get; }
+
+        public Message(Response response)
+        {
+            var indexOfSplit = response.Body.IndexOf("\r\n\r\n", StringComparison.Ordinal);
+            var octetsSplitText = "octets\r\n";
+
+            Header = response.Body.Remove(indexOfSplit);
+
+            SizeInOctets = int.Parse(Header.Remove(Header.IndexOf(" ", StringComparison.Ordinal)));
+            Header = Header.Substring(Header.IndexOf(octetsSplitText, StringComparison.Ordinal) + octetsSplitText.Length);
+
+            Body = response.Body.Substring(indexOfSplit + "\r\n\r\n".Length);
+        }
 
         public Message(MessageInfo info)
         {
